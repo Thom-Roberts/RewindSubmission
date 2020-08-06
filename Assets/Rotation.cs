0f3705c0e8c555eadbb8f7f429783cff7b282fb5
Thomas Roberts <robertsorange@gmail.com>
2020-08-05 23:56:11 -0600

    Added tutorial text

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -6,12 +6,9 @@ public class Rotation : MonoBehaviour {
     [Range(0f, 2f)]
     public float rotationTime = 1f;
 
-    private bool rotating = false;
-
     void Update() {
         Mouse mouse = InputSystem.GetDevice<Mouse>();
         if(mouse.leftButton.wasPressedThisFrame) {
-            Debug.Log("Mouse pressed");
             var ray = Camera.main.ScreenPointToRay(mouse.position.ReadValue());
             if(Physics.Raycast(ray, out var hit)) {
                 if(hit.transform.CompareTag("Rotater")) {
@@ -23,24 +20,12 @@ public class Rotation : MonoBehaviour {
             }
         }
         else if(mouse.leftButton.wasReleasedThisFrame) {
-            Debug.Log("Mouse released");
             RotateDown();
         }
-
-        /*if(rotating) {
-            Vector3 to = transform.rotation.eulerAngles;
-            to.z = rotationAngle;
-            transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
-        }*/
     }
 
     void RotateUp() {
-        rotating = true;
         LeanTween.rotate(gameObject, new Vector3(transform.rotation.x, transform.rotation.y, rotationAngle), rotationTime);
-        /*
-        LeanTween.value(gameObject, (val) => {
-            transform.Rotate(transform.rotation.x, transform.rotation.y, val);
-        }, transform.rotation.z, rotationAngle, timeToRotate);*/
     }
 
     void RotateDown() {
--- Assets/Scripts/Level1Controls.cs
+++ Assets/Scripts/Level1Controls.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Level1Controls : MonoBehaviour
 {
     public GameObject ball;
     public float startSpeed = 1f;
-
+    public TextMeshProUGUI tutorialText;
+    public float timeToHide = 2f;
+    
     private Transform startingBallTransform;
 
     void Start() {
         startingBallTransform = ball.transform;
         ball.GetComponent<Rigidbody>().velocity = startSpeed * Vector3.right;
+
+        StartCoroutine(hideTutorialText());
     }
 
     void Reset() {
         ball.transform.position = startingBallTransform.position;
         ball.transform.rotation = startingBallTransform.rotation;
+    }
+
+    IEnumerator hideTutorialText() {
+        yield return new WaitForSeconds(timeToHide);
+        tutorialText.GetComponent<CanvasGroup>().LeanAlpha(0f, timeToHide);
     }
 }
