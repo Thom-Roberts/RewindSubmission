a3fcec0d019b39affea525ca04f6dfd010c7d97f
Thomas Roberts <robertsorange@gmail.com>
2020-08-07 10:51:11 -0600

    Created velocity changer

---
+++ Assets/Scripts/FlipVelocity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipVelocity : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        print(collision.tag);
+        // Flips the velocity
+        if (collision.CompareTag("Ball")) {
+            collision.GetComponent<Rigidbody2D>().velocity *= (Vector3.right * -1);
+        }
+    }
+}
