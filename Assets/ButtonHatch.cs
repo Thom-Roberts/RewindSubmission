58322c60ff62e27ca5cfbd421a2db45ba075c632
Thomas Roberts <robertsorange@gmail.com>
2020-08-07 10:25:31 -0600

    Added button to control hatch

---
+++ Assets/ButtonHatch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHatch : MonoBehaviour
+{
+    public GameObject rotater;
+    public float depressDistance = 0.21f;
+    public float rotationAngle = 135f;
+    public float rotationTime = 0.5f;
+    private bool pressed = false;
+    private bool rotating = false;
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.CompareTag("Ball") && !rotating) {
+            rotating = true;
+            pressButton();
+
+            if (pressed)
+                rotateDown();
+            else
+                rotateUp();
+        }
+    }
+
+    private void pressButton() {
+        LeanTween.moveLocalY(gameObject, transform.position.y - depressDistance, rotationTime).setOnComplete(() => {
+            LeanTween.moveLocalY(gameObject, transform.position.y + depressDistance, rotationTime);
+        });
+    }
+
+    private void rotateUp() {
+        rotating = true;
+        LeanTween.rotate(rotater, new Vector3(transform.rotation.x, transform.rotation.y, rotationAngle), rotationTime).setOnComplete(() => {
+            pressed = true;
+            rotating = false;
+        });
+    }
+
+    private void rotateDown() {
+        rotating = true;
+        LeanTween.rotate(rotater, new Vector3(transform.rotation.x, transform.rotation.y, 0f), rotationTime).setOnComplete(() => {
+            pressed = true;
+            rotating = false;
+        });
+    }
+}
